e27d5f8bd9b4ba2bbfd71ce2246e40dce9270bac
Skaikru <dante0518@outlook.com>
2025-04-22 14:45:41 +0200

    initial commit - use this as template

---
+++ webpack.renderer.config.js
@@ -0,0 +1,56 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const config = require('./webpack.base.config');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const rendererConfig = { ...config };
+rendererConfig.target = 'electron-renderer';
+rendererConfig.entry = {
+  renderer: './src/renderer/renderer.ts',
+  preload: './src/preload/preload.ts',
+  exclusive: './src/renderer/exclusive.ts',
+};
+
+rendererConfig.plugins.push(
+  new CopyWebpackPlugin({
+    patterns: [
+      {
+        from: path.resolve(__dirname, 'src/renderer/app/dist'),
+        to: path.resolve(__dirname, 'dist/renderer/app/dist'),
+      },
+    ],
+  }),
+);
+
+rendererConfig.plugins.push(
+  new HtmlWebpackPlugin({
+    template: './src/renderer/index.html',
+    filename: path.join(__dirname, './dist/renderer/index.html'),
+    chunks: ['renderer'],
+    publicPath: '',
+    inject: false,
+  }),
+);
+
+rendererConfig.plugins.push(
+  new HtmlWebpackPlugin({
+    template: './src/renderer/osr.html',
+    filename: path.join(__dirname, './dist/renderer/osr.html'),
+    inject: false,
+    templateParameters: {
+      viteAssetsPath: 'app/dist/assets',
+    },
+  }),
+);
+
+rendererConfig.plugins.push(
+  new HtmlWebpackPlugin({
+    template: './src/renderer/exclusive.html',
+    filename: path.join(__dirname, './dist/exclusive/exclusive.html'),
+    chunks: ['exclusive'],
+    inject: false,
+  }),
+);
+
+module.exports = rendererConfig;
